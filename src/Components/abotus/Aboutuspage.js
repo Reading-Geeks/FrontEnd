c3bb2d051595b52dd37d24e9c3b07bb6ef8874ab
Ahmad Jallad <ahmadjlad@hotmail.com>
2021-09-21 19:25:02 +0300

    fix some errors in aboutUs Page

--- src/Components/abotus/Aboutuspage.js
+++ src/Components/abotus/Aboutuspage.js
@@ -1,170 +1,32 @@
 import React, { Component } from "react";
-import { Card, Button } from "react-bootstrap";
-// import axios from "axios";
-// import { withAuth0 } from "@auth0/auth0-react";
-import Col from 'react-bootstrap/Col';
-import "./styleaboutus.css"
-
+import { Button } from "react-bootstrap";
 class Aboutuspage extends Component {
   render() {
-    console.log(this.props.email)
-    return ( <>
-
-      {/* <h1>{this.props.item.status}</h1>
-<div class="cards">
-  <div class="card">
-    <h2 class="card-title">{this.props.item.PersonName}</h2>
-    <img src={this.props.item.image} alt=""  height='500'/>
-    <p class="card-desc">{this.props.item.Description}</p>
-    <p class="card-desc">{this.props.item.email}</p>
-  </div>
-  {(this.props.email === "readinggeeks301@gmail.com") ? <Button variant="primary" onClick={() => this.props.showUpdateform(this.props.item)}>Update</Button> : null }
-</div> */}
-
-
-
-      {/* <Col>
-<div class="center">
-  <div class="property-card">
-    <a href="#">
-     <div class="property-image">
-        <div class="property-image-title">
-      <img src={this.props.item.image} alt=""      height="400"   width="300"               />
-      </div></div></a>
-    <div class="property-description">
-      <h5> {this.props.item.PersonName} </h5>
-      <p>{this.props.item.Description}</p>
-      <p>  {this.props.item.email}</p>
-    </div>
-   
-      
-      <div >
-      {(this.props.email === "readinggeeks301@gmail.com") ? <Button variant="primary" onClick={() => this.props.showUpdateform(this.props.item)}>Update</Button> : null }
-    
-   
-       </div>
-  
-  </div>
-</div></Col> */}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-      <html lang="en">
-
-        <head>
-          <title>{this.props.item.PersonName}</title>
-
-        </head>
-
-        <body>
-        <div class="wrapper">
-          <div class="product-img">
-            <img  src={this.props.item.image}  height="420" width="327" />
+    console.log(this.props.email);
+    return (
+      <div class="wrapper">
+        <div class="product-img">
+          <img src={this.props.item.image} height="420" width="327" />
+        </div>
+        <div class="product-info">
+          <div class="product-text">
+            <h1>{this.props.item.PersonName}</h1>
+            <h2></h2>
+            <p>{this.props.item.Description}</p>
           </div>
-          <div class="product-info">
-            <div class="product-text">
-              <h1>{this.props.item.PersonName}</h1>
-              <h2></h2>
-              <p>{this.props.item.Description}</p>
-            </div>
-            <div class="product-price-btn">
-            {(this.props.email === "readinggeeks301@gmail.com") ? <Button variant="primary" onClick={() => this.props.showUpdateform(this.props.item)}>Update</Button> : null }
-    
-            </div>
+          <div class="product-price-btn">
+            {this.props.email === "readinggeeks301@gmail.com" ? (
+              <Button
+                variant="primary"
+                onClick={() => this.props.showUpdateform(this.props.item)}
+              >
+                Update
+              </Button>
+            ) : null}
           </div>
         </div>
-        </body>
-</html>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        {/* <Col>
-          <Card style={{ width: '18rem' }}>
-            <Card.Body>
-              <Card.Text>
-                <h1>{this.props.item.PersonName}</h1>
-                <img
-                  className="d-block w-100"
-                  src={this.props.item.image}
-                  alt="First slide"
-                  height='200'
-                />
-                <h3>{this.props.item.Description}</h3>
-                <h3>{this.props.item.email}</h3>
-              </Card.Text>
-              
-              {(this.props.email === "readinggeeks301@gmail.com") ? <Button variant="primary" onClick={() => this.props.showUpdateform(this.props.item)}>Update</Button> : null }
-            </Card.Body>
-            <Card.Header>{this.props.item.status}</Card.Header>
-          </Card>
-        </Col> */}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-      </>
-
-
-
-      )
+      </div>
+    );
   }
 }
-      export default Aboutuspage;
+export default Aboutuspage;
--- src/Components/abotus/UpdateFromModel.js
+++ src/Components/abotus/UpdateFromModel.js
@@ -1,39 +1,55 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Modal from 'react-bootstrap/Modal';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form'
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 class UpdateFromModel extends React.Component {
-    render() {
-        return (
-            <> 
-                <Modal show={this.props.show} onHide={this.props.closeModel}>
-                    <Modal.Header closeButton>
-                        <Modal.Title className="title">update About Us  </Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>
-                        <Form onSubmit={this.props.ubdateInfo}>
-                            <Form.Group className="mb-3" controlId="formBasicEmail">
-                                <Form.Label>Edit Name </Form.Label>
-                                <Form.Control type="text" name='PersonName' defaultValue={this.props.PersonName} />
-                            </Form.Group>
-                            <Form.Group className="mb-3" controlId="formBasicEmail">
-                                <Form.Label>Edit Image</Form.Label>
-                                <Form.Control type="text" name='image' defaultValue={this.props.image} />
-                            </Form.Group>
-                            <Form.Group className="mb-3" controlId="formBasicEmail">
-                                <Form.Label>Edit description</Form.Label>
-                                <Form.Control type="text" name='Description' defaultValue={this.props.Description} />
-                            </Form.Group>
-                            <Modal.Footer>
-                                <Button variant="secondary" onClick={this.props.closeModel}>Close</Button>
-                                <Button variant="primary" type='submit'>Update</Button>
-                            </Modal.Footer>
-                        </Form>
-                    </Modal.Body>
-                </Modal>
-            </>
-        )
-    }
+  render() {
+    return (
+      <>
+        <Modal show={this.props.show} onHide={this.props.closeModel}>
+          <Modal.Header closeButton>
+            <Modal.Title className="title">update About Us </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form onSubmit={this.props.ubdateInfo}>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Edit Name </Form.Label>
+                <Form.Control
+                  type="text"
+                  name="PersonName"
+                  defaultValue={this.props.PersonName}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Edit Image</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="image"
+                  defaultValue={this.props.image}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Edit description</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="Description"
+                  defaultValue={this.props.Description}
+                />
+              </Form.Group>
+              <Modal.Footer>
+                <Button variant="secondary" onClick={this.props.closeModel}>
+                  Close
+                </Button>
+                <Button variant="primary" type="submit">
+                  Update
+                </Button>
+              </Modal.Footer>
+            </Form>
+          </Modal.Body>
+        </Modal>
+      </>
+    );
+  }
 }
 export default UpdateFromModel;
